fba89e9ba2e9d6160b6651b0fdd32318e8f69b01
William Baillargeon <baillargeon.william@oricom.ca>
2021-09-27 14:13:55 -0400

    rocketelevatorsdigitalpresence

---
+++ assets/js/quote.js
@@ -0,0 +1,18 @@
+quoteFormElements: [
+    {
+        type: "residential",
+        inputFields: ["n-apartments-res", "n-floors-res", "n-basements-res"]
+    },
+    {
+        type: "commercial",
+        inputFields: ["number-of-floors", "number-of-basements","number-of-companies", "number-of-parking-spots", "number-of-elevators"]
+    },
+    {
+        type: "corporate",
+        inputFields: ["number-of-floors", "number-of-basements","number-of-parking-spots", "number-of-corporations", "maximum-occupancy"]
+    },
+    {
+        type: "hybrid",
+        inputFields: ["number-of-floors", "number-of-basements", "number-of-companies", "number-of-parking-spots", "maximum-occupancy", "business-hours"]
+    }
+]
