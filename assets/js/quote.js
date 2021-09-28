6368262d28e4856959ca07560e70444a2ecaf27e
William Baillargeon <baillargeon.william@oricom.ca>
2021-09-27 20:20:13 -0400

    rocketelevatorsdigitalpresence

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,1 +1,29 @@
-var showres = document.getElementById("")
+$("#select-select").hide();
+
+function showDiv(value) {
+    if (value=="residential") {
+            document.getElementById("residential-f").style.display="block";
+            document.getElementById("commercial-f").style.display="none";
+            document.getElementById("corporate-f").style.display="none";
+            document.getElementById("hybrid-f").style.display="none";
+    }
+    if (value=="commercial") {
+        document.getElementById("residential-f").style.display="none";
+        document.getElementById("commercial-f").style.display="block";
+        document.getElementById("corporate-f").style.display="none";
+        document.getElementById("hybrid-f").style.display="none";
+    }
+    if (value=="corporate") {
+        document.getElementById("residential-f").style.display="none";
+        document.getElementById("commercial-f").style.display="none";
+        document.getElementById("corporate-f").style.display="block";
+        document.getElementById("hybrid-f").style.display="none";
+    }
+    if (value=="hybrid") {
+        document.getElementById("residential-f").style.display="none";
+        document.getElementById("commercial-f").style.display="none";
+        document.getElementById("corporate-f").style.display="none";
+        document.getElementById("hybrid-f").style.display="block";
+    }
+
+}
