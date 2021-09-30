ead0f8fff70c29e51dd87676eef2572c98392671
William Baillargeon <baillargeon.william@oricom.ca>
2021-09-29 23:40:25 -0400

    rocketelevatorsdigitalpresence

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -28,7 +28,7 @@ function showDiv(value) {
 
 }
 
-    //commercial calcul
+    //commercial calcul works
 
     function calcCom(value){
         var elevatorsNeeded;
@@ -38,7 +38,7 @@ function showDiv(value) {
 
     }
 
-    //residential calcul
+    //residential calcul manque à faire x2 elevatorneededres chaque fois qu'il y a un column+1
 
     function  calcRes() {
 
@@ -52,16 +52,13 @@ function showDiv(value) {
         var elevatorsNeededRes = document.getElementById("elevator-amount-f").value = Math.ceil((numberApartmentsRes / numberFloorsRes) / 6);
         console.log(elevatorsNeededRes);
 
-        var columnRes = Math.ceil(numberFloorsRes / 20);
-        console.log(columnRes, "nombre de columns res");
-
+        var columnRes;
+        columnRes = Math.ceil(numberFloorsRes / 20);
+        console.log(columnRes, 'nombre de column res');
     }
 
-    //elevatorneededres *2 chaque fois qu'il y a une column
-
-    //corporate hybrid calcul
-
-    function calcCorHyb() {
+    //calcul corporate *works*
+    function calcCor() {
 
         var numberFloorsCor = document.getElementById("n-floors-cor").value;
         console.log(numberFloorsCor, "nombre de floors cor");
@@ -72,10 +69,60 @@ function showDiv(value) {
         var maxOccupancyCor = document.getElementById("max-occupancy-cor").value;
         console.log(maxOccupancyCor, "max occupancy cor");
 
-        maxOccupancyCor * numberFloorsCor, numberBasementCor;
+        var floorBasement = +numberFloorsCor + +numberBasementCor; // floor + basement
+        console.log(floorBasement, "floor + basement");
 
-        var elevatorsNeededCor = document.getElementById("elevator-amount-f").value = Math.ceil(maxOccupancyCor / 1000);
-        console.log(elevatorsNeededCor, "nombre elevator needed");
+        var totalOccupants = maxOccupancyCor * floorBasement; // n total d'occupants
+        console.log(totalOccupants, "n total occupants");
+
+        var elevatorsNeededCor = totalOccupants / 1000; // elevator needed
+        console.log(elevatorsNeededCor, "elevator needed");
+
+        var elevatorColumnsCor = Math.ceil(floorBasement / 20); // nombre de column d'elevator
+        console.log(elevatorColumnsCor, "nombre de column d'elevator");
+
+        var elevatorPerColumnCor = Math.ceil(elevatorsNeededCor / elevatorColumnsCor); // nombre de elevator par column
+        console.log(elevatorPerColumnCor, "nombre de elevator par column");
+
+        var totalElevatorsNeededCor = Math.ceil(elevatorPerColumnCor * elevatorColumnsCor); //total elevator (champ Amount of elevators needed)
+        console.log(totalElevatorsNeededCor, "nombre total de elevators needed");
+
+        document.getElementById("elevator-amount-f").value = totalElevatorsNeededCor;
+
+    }
+
+    //hybrid
+    function calcHyb() {
+
+        var numberFloorsHyb = document.getElementById("n-floors-hyb").value;
+        console.log(numberFloorsHyb, "nombre de floors hyb");
+
+        var numberBasementHyb = document.getElementById("n-basement-hyb").value;
+        console.log(numberBasementHyb, "nombre basement hyb");
+
+        var maxOccupancyHyb = document.getElementById("n-max-occupancy-hyb").value;
+        console.log(maxOccupancyHyb, "max occupancy hyb");
+
+        var floorBasement = +numberFloorsHyb + +numberBasementHyb; // floor + basement
+        console.log(floorBasement, "floor + basement");
+
+        var totalOccupants = maxOccupancyHyb * floorBasement; // n total d'occupants
+        console.log(totalOccupants, "n total occupants");
+
+        var elevatorsNeededHyb = totalOccupants / 1000; // elevator needed
+        console.log(elevatorsNeededHyb, "elevator needed");
+
+        var elevatorColumns = Math.ceil(floorBasement / 20); // nombre de column d'elevator
+        console.log(elevatorColumns, "nombre de column d'elevator");
+
+        var elevatorPerColumn = Math.ceil(elevatorsNeededHyb / elevatorColumns); // nombre de elevator par column
+        console.log(elevatorPerColumn, "nombre de elevator par column");
+
+        var totalElevatorsNeededHyb = Math.ceil(elevatorPerColumn * elevatorColumns); //total elevator (champ Amount of elevators needed)
+        console.log(totalElevatorsNeededHyb, "nombre total de elevators needed");
+
+        document.getElementById("elevator-amount-f").value = totalElevatorsNeededHyb;
+
     }
 
     // number of occupants X number of floors, basements to obtain total number of occupants OK
